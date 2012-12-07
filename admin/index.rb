027ee9f085ad58886ceb5f75d29a0bf9033267fb
7rans <transfire@gmail.com>
2012-12-07 15:06:18 -0500

    Let try an index ruby script in admin directory. [admin]

---
+++ admin/index.rb
@@ -0,0 +1,36 @@
+
+name    "anise"
+version "0.7.0"
+
+title   "Anise"
+summary "Dynamic Annotation System"
+
+description "Anise is an annotations systems for the Ruby programming lanaguage."
+
+authors "trans <transfire@gmail.com>"
+
+requirements(
+  "qed     (test)",
+  "ae      (test)",
+  "citron  (test)",
+  "detroit (build)"
+)
+
+resources(
+  "home" => "http://rubyworks.github.com/anise",
+  "code" => "http://github.com/rubyworks/anise",
+  "bugs" => "http://github.com/rubyworks/anise/issues",
+  "chat" => "http://chat.us.freenode.net/rubyworks",
+  "mail" => "http://groups.google.com/groups/rubyworks-mailinglist"
+)
+
+repositories(
+  "upstream" => "http://github.com/rubyworks/anise.git"
+)
+
+organization "Rubyworks"
+
+created "2008-02-21"
+
+copyright "2008 Rubyworks (BSD-2-Clause)"
+
